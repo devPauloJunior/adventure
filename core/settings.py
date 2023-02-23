1ce1de55fd1a51cbfd33a48f0718efc52f1b701e
Paulo Junior <npaulo.ce@gmail.com>
2023-02-23 17:54:06 -0300

    Update for MIGRATE

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'plans',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'adventure',
+        'USER': 'user_con',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
     }
 }
 
