ccf98f86ec9679c875ad63d1845572f78dd85389
Paulo Junior <npaulo.ce@gmail.com>
2023-02-24 21:28:56 -0300

    Models Views For Index

--- plans/admin.py
+++ plans/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Plans
 
-# Register your models here.
+class ListPlans(admin.ModelAdmin):
+    list_display = ('titulo', 'type_plan', 'value')
+
+admin.site.register(Plans, ListPlans)
